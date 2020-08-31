89056383039acf30c1b908a680f6d1847f3fad62
ayush14102 <190773116009@socet.edu.in>
2020-08-31 18:27:31 +0530

    First module4-solution

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,12 +1,12 @@
 /***Module4-solution***/
 
 
-var names = ["Alex", "Marisol", "Joe","Joshep"];
+var names = ["Yaakov", "John", " Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 // console.log(names);
 
 for (var i = 0; i < names.length; i++) {
 	if (names[i][0]=='j' || names[i][0]=='J'){
-	console.log("GoodBye " +names[i]);	
+	console.log("Good Bye " +names[i]);	
 	}
 	else{
 		console.log("Hello " +names[i]);
